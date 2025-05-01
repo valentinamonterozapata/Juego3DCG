2349e2cb2592f55da5ac0cf03a24b41eb948e620
valentinamonterozapata <valentina.montero@uao.edu.co>
2025-04-30 23:11:34 -0500

    Ajustes en mi escena (playe y enemigo) agregue componentes y scripts

---
+++ Prueba Asset valentina M/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+    }
+}
