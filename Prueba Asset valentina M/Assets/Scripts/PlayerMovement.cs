2e3f3092140d368cf679d08e74f7cc24ace2ed8f
valentinamonterozapata <valentina.montero@uao.edu.co>
2025-05-02 15:58:15 -0500

    Le díi movimiento al personaje, el Zombie se mueve atrás de él,, la cámara persigue al personaje

--- Prueba Asset valentina M/Assets/Scripts/PlayerMovement.cs
+++ Prueba Asset valentina M/Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float speedRotation = 200.0f; // Velocidad de rotación
+    private Animator anim;
+    private float x, y;
     private Rigidbody rb;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * speedRotation, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * speed);
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
