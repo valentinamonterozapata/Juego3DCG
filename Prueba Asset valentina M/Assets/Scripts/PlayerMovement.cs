04a68c24a000f610242cca91be6f638147cfb323
EstebanCcorrea <estebancho8@hotmail.com>
2025-05-20 00:01:40 -0500

    Se hacen cambios

--- Prueba Asset valentina M/Assets/Scripts/PlayerMovement.cs
+++ Prueba Asset valentina M/Assets/Scripts/PlayerMovement.cs
@@ -1,145 +1,104 @@
 using UnityEngine;
 
-
-
 public class PlayerMovement : MonoBehaviour
-
 {
-
     public float walkSpeed = 3f;
-
     public float runSpeed = 47f;
-
     public float jumpForce = 5f;
-
     public float rotationSpeed = 200f;
 
-
+    int isBackwalkHash;
     int isWalkingHash;
     int isRunningHash;
+
     private Animator anim;
-
     private Rigidbody rb;
-
     private bool isGrounded;
-
     private float currentSpeed;
 
-
-
     void Start()
-
     {
-
         anim = GetComponent<Animator>();
-
         rb = GetComponent<Rigidbody>();
 
-
+        isBackwalkHash = Animator.StringToHash("isBackwalk");
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
     }
 
-
-
     void Update()
-
     {
         bool isRunning = anim.GetBool(isRunningHash);
         bool isWalking = anim.GetBool(isWalkingHash);
-        bool forwadPressed = Input.GetKey("w");
+        bool isBackwalk = anim.GetBool(isBackwalkHash);
+
+        bool backwalkPressed = Input.GetKey("s");
+        bool forwardPressed = Input.GetKey("w");
         bool runPressed = Input.GetKey("left shift");
 
-
-        // Movimiento básico
-
         float moveVertical = Input.GetAxis("Vertical");
-
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        if (!isWalking && forwadPressed)
+        // Caminata hacia adelante
+        if (!isWalking && forwardPressed)
         {
             anim.SetBool(isWalkingHash, true);
         }
-        if (isWalking && !forwadPressed)
+        if (isWalking && !forwardPressed)
         {
             anim.SetBool(isWalkingHash, false);
         }
 
-
+        // Caminata hacia atrás
+        if (!isBackwalk && backwalkPressed)
+        {
+            anim.SetBool(isBackwalkHash, true);
+        }
+        if (isBackwalk && !backwalkPressed)
+        {
+            anim.SetBool(isBackwalkHash, false);
+        }
 
         // Rotación
-
         transform.Rotate(0, moveHorizontal * rotationSpeed * Time.deltaTime, 0);
 
-
-
-        // Verificar si está corriendo
-
-
-
-        currentSpeed = isRunning ? runSpeed : walkSpeed;
-
-        if (!isRunning && (forwadPressed && runPressed))
+        // Correr hacia adelante solamente
+        if (!isRunning && (forwardPressed && runPressed))
         {
             anim.SetBool(isRunningHash, true);
         }
-        if (isRunning && (!forwadPressed || !runPressed))
+        if (isRunning && (!forwardPressed || !runPressed))
         {
             anim.SetBool(isRunningHash, false);
         }
 
+        // Determinar velocidad actual
+        currentSpeed = isRunning ? runSpeed : walkSpeed;
 
         // Movimiento
-
         if (moveVertical != 0)
-
         {
-
-            transform.Translate(0, 0, moveVertical * currentSpeed * Time.deltaTime);
-
+            // Evitar correr hacia atrás
+            if (!(moveVertical < 0 && runPressed))
+            {
+                transform.Translate(0, 0, moveVertical * currentSpeed * Time.deltaTime);
+            }
         }
-
-
-
-        // Configurar parámetros de animación
-
-
-
-
-
-
 
         // Salto
-
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-
         {
-
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
             anim.SetTrigger("Jump");
-
             isGrounded = false;
-
         }
-
     }
-
-
 
     void OnCollisionEnter(Collision collision)
-
     {
-
         if (collision.gameObject.CompareTag("Ground"))
-
         {
-
             isGrounded = true;
-
         }
-
     }
-
 }
